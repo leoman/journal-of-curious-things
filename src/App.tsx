e8689d79cd4f4c612be1c7569e879d331988915a
Peter Mansell <peter@pmansell.com>
2021-01-24 11:43:45 +0000

    Adding admin redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
-import React from "react"
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
-import { Provider } from 'react-redux';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { Provider } from "react-redux";
 
-import store from './redux';
+import store from "./redux";
 
-import AdminPostsList from './admin/views/Posts/list'
-import AdminPostsCreate from './admin/views/Posts/create'
-import AdminPostsEdit from './admin/views/Posts/edit'
+import AdminPostsList from "./admin/views/Posts/list";
+import AdminPostsCreate from "./admin/views/Posts/create";
+import AdminPostsEdit from "./admin/views/Posts/edit";
 
-import AdminProductsList from './admin/views/Products/list'
-import AdminProductsCreate from './admin/views/Products/create'
-import AdminProductsEdit from './admin/views/Products/edit'
+import AdminProductsList from "./admin/views/Products/list";
+import AdminProductsCreate from "./admin/views/Products/create";
+import AdminProductsEdit from "./admin/views/Products/edit";
 
-import AdminThemesList from './admin/views/Themes/list'
-import AdminThemesCreate from './admin/views/Themes/create'
-import AdminThemesEdit from './admin/views/Themes/edit'
+import AdminThemesList from "./admin/views/Themes/list";
+import AdminThemesCreate from "./admin/views/Themes/create";
+import AdminThemesEdit from "./admin/views/Themes/edit";
 
-import AdminGalleryList from './admin/views/Gallery/list'
+import AdminGalleryList from "./admin/views/Gallery/list";
 
-import PostsList from './views/posts/list'
-import PostView from './views/posts/view'
+import PostsList from "./views/posts/list";
+import PostView from "./views/posts/view";
 
-import ProductsList from './views/products/list'
-import ProductView from './views/products/view'
+import ProductsList from "./views/products/list";
+import ProductView from "./views/products/view";
 
-import { AppWrapper } from './styles'
+import { AppWrapper } from "./styles";
 
 const App = () => (
   <Provider store={store}>
@@ -32,28 +37,59 @@ const App = () => (
       <Router>
         <Switch>
           <Route path="/admin/posts/preview/:slug" exact component={PostView} />
-          <Route path="/admin/posts/edit/:id" exact component={AdminPostsEdit} />
-          <Route path="/admin/posts/create" exact component={AdminPostsCreate} />
+          <Route
+            path="/admin/posts/edit/:id"
+            exact
+            component={AdminPostsEdit}
+          />
+          <Route
+            path="/admin/posts/create"
+            exact
+            component={AdminPostsCreate}
+          />
           <Route path="/admin/posts" exact component={AdminPostsList} />
-          <Route path="/admin/products/preview/:slug" exact component={ProductView} />
-          <Route path="/admin/products/edit/:id" exact component={AdminProductsEdit} />
-          <Route path="/admin/products/create" exact component={AdminProductsCreate} />
+          <Route
+            path="/admin/products/preview/:slug"
+            exact
+            component={ProductView}
+          />
+          <Route
+            path="/admin/products/edit/:id"
+            exact
+            component={AdminProductsEdit}
+          />
+          <Route
+            path="/admin/products/create"
+            exact
+            component={AdminProductsCreate}
+          />
           <Route path="/admin/products" exact component={AdminProductsList} />
-          <Route path="/admin/themes/edit/:id" exact component={AdminThemesEdit} />
-          <Route path="/admin/themes/create" exact component={AdminThemesCreate} />
+          <Route
+            path="/admin/themes/edit/:id"
+            exact
+            component={AdminThemesEdit}
+          />
+          <Route
+            path="/admin/themes/create"
+            exact
+            component={AdminThemesCreate}
+          />
           <Route path="/admin/themes" exact component={AdminThemesList} />
           <Route path="/admin/gallery" exact component={AdminGalleryList} />
+          <Route path="/admin">
+            <Redirect to="/admin/posts" />
+          </Route>
           <Route path="/products/:slug" exact component={ProductView} />
           <Route path="/products" exact component={ProductsList} />
           <Route path="/posts/:slug" exact component={PostView} />
           <Route path="/" exact component={PostsList} />
           <Route>
-            <Redirect to="/"/>
+            <Redirect to="/" />
           </Route>
         </Switch>
       </Router>
     </AppWrapper>
   </Provider>
-)
+);
 
 export default App;
